eed42942df85ef4c5d8ff0d44e9bfe70e1cc4964
nathanbelt23UniAndes <111519973+nathanbelt23UniAndes@users.noreply.github.com>
2023-09-27 17:51:06 -0500

    Version inicial de well alert

---
+++ assets/auxiliarMetodos.js
@@ -0,0 +1,48 @@
+function cerrarSession() {
+  window.location.href = "cerrarSesion.html";
+}
+
+
+function home() {
+  window.location.href = "index.html";
+}
+
+function login() {
+  window.location.href = "login.html";
+}
+
+
+
+function registro() {
+  window.location.href = "registro.html";
+}
+
+
+
+function alarmas() {
+  window.location.href = "alarmas.html";
+}
+
+
+function inicio() {
+  window.location.href = "inicio.html";
+}
+
+
+
+
+function borrarAlarma() {
+  window.location.href = "eliminaralarma.html";
+}
+
+function editarAlarma() {
+  window.location.href = "editaralarma.html";
+}
+
+function crearAlarma() {
+  window.location.href = "crearAlarma.html";
+}
+
+
+
+
